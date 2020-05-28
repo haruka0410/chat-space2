e7bb3fcc286b69357559f6b11d006a63ab4af38c
haruka0410 <haruka.maruyama@di-v.co.jp>
2020-05-28 13:17:16 +0900

    Revert "message-registration"
    This reverts commit d42159af1f91d833ad1ca9061178365eb0f4ddee.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "groups#index"
+  root "messages#index"
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update, :index] do
-    resources :messages, only: [:index, :create]
-  end
 end
